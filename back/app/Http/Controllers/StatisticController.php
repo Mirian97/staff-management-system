5e1909b7154e3c05fec8185869762e606d24ab99
Mirian Quispe <mirianquispe1505@gmail.com>
2025-01-10 19:56:36 -0300

    feat: return counts

---
+++ back/app/Http/Controllers/StatisticController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Department;
+use App\Models\Employee;
+use App\Models\Task;
+
+class StatisticController extends Controller
+{
+    public function counts()
+    {
+        $employee_count = Employee::count();
+        $task_count = Task::count();
+        $department_count = Department::count();
+
+        return response()->json([
+            "employee_count" => $employee_count,
+            "task_count" => $task_count,
+            "department_count" => $department_count,
+        ]);
+    }
+}
