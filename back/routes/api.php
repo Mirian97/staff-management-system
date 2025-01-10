94398baaaaff31810c87b55bd0cd8b53ecb3c585
Mirian Quispe <mirianquispe1505@gmail.com>
2025-01-10 19:57:57 -0300

    fix: routes seguiments

--- back/routes/api.php
+++ back/routes/api.php
@@ -2,19 +2,27 @@
 
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\JWTAuthController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\StatisticController;
 use App\Http\Controllers\TaskController;
-use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [JWTAuthController::class, 'register']);
-Route::post('login', [JWTAuthController::class, 'login']);
-Route::middleware([JwtMiddleware::class])->group(function () {
-    Route::get('user', [JWTAuthController::class, 'getUser']);
-    Route::post('logout', [JWTAuthController::class, 'logout']);
+Route::get('ping', function () {
+    return 'pong';
 });
-Route::apiResources([
-    'departments' => DepartmentController::class,
-    'employees' => EmployeeController::class,
-    'tasks' => TaskController::class,
-]);
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [EmployeeController::class, 'store']);
+
+Route::middleware("auth")->group(function () {
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::get("statistic", [StatisticController::class, "counts"]);
+
+    Route::apiResources([
+        'departments' => DepartmentController::class,
+        'employees' => EmployeeController::class,
+        'tasks' => TaskController::class,
+    ]);
+});
