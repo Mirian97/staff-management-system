892bf23389c7e9a952b9d4a09fb8a4c50dd9577a
Mirian Quispe <mirianquispe1505@gmail.com>
2025-02-03 19:57:52 -0300

    feat: set relation employee to task

--- back/app/Http/Controllers/TaskController.php
+++ back/app/Http/Controllers/TaskController.php
@@ -12,7 +12,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::paginate(10);
+        $tasks = Task::with("employee")->paginate(10);
 
         return response()->json($tasks);
     }
