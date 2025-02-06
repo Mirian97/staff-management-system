6dfb6dbdb33fd78a6086e26cf329ad9a3e4f48d3
Mirian Quispe <mirianquispe1505@gmail.com>
2025-02-05 21:58:12 -0300

    feat: improve useEmployees hook

--- front/app/_hooks/useEmployee.tsx
+++ front/app/_hooks/useEmployee.tsx
@@ -1,43 +1,66 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { employeeService, TEmployeePayload } from '../_service/employee-service';
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { toast } from "sonner";
+import { TEmployeeSchema } from "../_schemas/employee-schema";
+import { employeeService } from "../_service/employee-service";
 
-export const EMPLOYEES_QUERY_KEY = 'employees';
+export const EMPLOYEES_QUERY_KEY = "employees";
 
-export function useEmployee() {
+interface EmployeeHookProps {
+  closeDialog?: () => void;
+}
+
+export function useEmployee({ closeDialog }: EmployeeHookProps = {}) {
   const queryClient = useQueryClient();
+  const searchParams = useSearchParams();
+  const currentPage = searchParams?.get("page") ?? "1";
 
   const { data } = useQuery({
-    queryKey: [EMPLOYEES_QUERY_KEY],
-    queryFn: () => employeeService.getAll(),
+    queryKey: [EMPLOYEES_QUERY_KEY, currentPage],
+    queryFn: () => employeeService.getAll({ page: Number(currentPage) }),
+    select: (data) => ({
+      lastPage: data.last_page,
+      employees: data.data,
+    }),
   });
 
   const refetchData = () => {
-    queryClient.invalidateQueries({ queryKey: [EMPLOYEES_QUERY_KEY] })
-  }
+    queryClient.invalidateQueries({ queryKey: [EMPLOYEES_QUERY_KEY] });
+    closeDialog?.();
+  };
 
-  const { mutate: createDepartment } = useMutation({
-    mutationFn: (payload: TEmployeePayload) => employeeService.create(payload),
-    onSuccess: () => refetchData(),
-    onError: (error) => console.error('Erro ao criar departamento:', error),
+  const { mutate: createEmployee } = useMutation({
+    mutationFn: (payload: TEmployeeSchema) => employeeService.create(payload),
+    onSuccess: () => {
+      toast.success("Funcionário criado!");
+      refetchData();
+    },
+    onError: (error) => console.error("Erro ao criar departamento:", error),
   });
 
-  const { mutate: updateDepartment } = useMutation({
-    mutationFn: ({ id, payload }: { id: number; payload: TEmployeePayload }) =>
+  const { mutate: updateEmployee } = useMutation({
+    mutationFn: ({ id, payload }: { id: number; payload: TEmployeeSchema }) =>
       employeeService.update(id, payload),
-    onSuccess: () => refetchData(),
-    onError: (error) => console.error('Erro ao atualizar departamento:', error),
+    onSuccess: () => {
+      toast.success("Funcionário atualizadod!");
+      refetchData();
+    },
+    onError: (error) => console.error("Erro ao atualizar departamento:", error),
   });
 
-  const { mutate: deleteDepartment } = useMutation({
+  const { mutate: deleteEmployee } = useMutation({
     mutationFn: (id: number) => employeeService.delete(id),
-    onSuccess: () => refetchData(),
-    onError: (error) => console.error('Erro ao excluir departamento:', error),
+    onSuccess: () => {
+      toast.success("Funcionário criada!");
+      refetchData();
+    },
+    onError: (error) => console.error("Erro ao excluir departamento:", error),
   });
 
   return {
-    departments: data,
-    createDepartment,
-    updateDepartment,
-    deleteDepartment,
+    ...data,
+    createEmployee,
+    updateEmployee,
+    deleteEmployee,
   };
 }
