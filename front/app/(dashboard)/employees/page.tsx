aa5b76b96ab4368316768bc5c24ce160893ce9a3
Mirian Quispe <mirianquispe1505@gmail.com>
2025-02-05 21:50:49 -0300

    feat: consume employees from hook

--- front/app/(dashboard)/employees/page.tsx
+++ front/app/(dashboard)/employees/page.tsx
@@ -1,39 +1,14 @@
 "use client";
 import { DataTable } from "@/app/_components/ui/datatable";
 import { Input } from "@/app/_components/ui/input";
+import { useEmployee } from "@/app/_hooks/useEmployee";
 import { useState } from "react";
 import { AddEmployeeButton } from "./_components/add-employee-button";
 import { employeeColumns } from "./_components/employee-columns";
 
-const mockEmployees = [
-  {
-    id: 1,
-    firstName: "João",
-    lastName: "Silva",
-    email: "joao.silva@email.com",
-    phone: "(11) 99999-9999",
-    department: "TI",
-  },
-  {
-    id: 2,
-    firstName: "Maria",
-    lastName: "Santos",
-    email: "maria.santos@email.com",
-    phone: "(11) 88888-8888",
-    department: "RH",
-  },
-];
-
 const Employees = () => {
-  const [isNewDialogOpen, setIsNewDialogOpen] = useState(false);
   const [search, setSearch] = useState("");
-
-  const filteredEmployees = mockEmployees.filter(
-    (employee) =>
-      employee.firstName.toLowerCase().includes(search.toLowerCase()) ||
-      employee.lastName.toLowerCase().includes(search.toLowerCase()) ||
-      employee.email.toLowerCase().includes(search.toLowerCase())
-  );
+  const { employees, lastPage } = useEmployee();
 
   return (
     <div className="space-y-4">
@@ -46,7 +21,11 @@ const Employees = () => {
         />
         <AddEmployeeButton />
       </div>
-      <DataTable data={mockEmployees} columns={employeeColumns} />
+      <DataTable
+        data={employees || []}
+        columns={employeeColumns}
+        pageCount={lastPage}
+      />
     </div>
   );
 };
