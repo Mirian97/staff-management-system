79a8bb9744a3d79d9d5da2652ad225fdcc9b8bd5
Mirian Quispe <mirianquispe1505@gmail.com>
2025-02-05 21:56:43 -0300

    feat: include the assignee_name

--- front/app/_schemas/task-schema.ts
+++ front/app/_schemas/task-schema.ts
@@ -3,12 +3,18 @@ import * as z from "zod";
 export const taskSchema = z.object({
   title: z.string().min(2, "Título deve ter no mínimo 2 caracteres"),
   description: z.string().min(2, "Descrição deve ter no mínimo 2 caracteres"),
-  assignee_id: z.string().refine((val) => !isNaN(Number(val)) && Number(val) > 0, {
-    message: "O ID do responsável deve ser um número inteiro positivo",
-  }),
-  due_date: z.date().refine((date) => date > new Date(), {
-    message: "Data de vencimento deve ser posterior a hoje",
-  }),
+  assignee_id: z
+    .string()
+    .refine((val) => !isNaN(Number(val)) && Number(val) > 0, {
+      message: "O ID do responsável deve ser um número inteiro positivo",
+    }),
+  assignee_name: z.string().min(2, "Selecione um funcionário"),
+  due_date: z
+    .date()
+    .refine((date) => date > new Date(), {
+      message: "Data de vencimento deve ser posterior a hoje",
+    })
+    .transform((date) => date.toISOString().slice(0, 10)),
 });
 
-export type TTaskSchema = z.infer<typeof taskSchema>
+export type TTaskSchema = z.infer<typeof taskSchema>;
