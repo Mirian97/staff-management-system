9f0508d0f99bb3568a0592966039a178ddcea460
Mirian Quispe <mirianquispe1505@gmail.com>
2025-02-05 21:51:49 -0300

    feat: update employee columns

--- front/app/(dashboard)/employees/_components/employee-columns.tsx
+++ front/app/(dashboard)/employees/_components/employee-columns.tsx
@@ -1,18 +1,12 @@
 "use client";
+import { TEmployee } from "@/app/_service/employee-service";
 import { ColumnDef } from "@tanstack/react-table";
 import { DeleteEmployeeButton } from "./delete-employee-button";
 import { EditEmployeeButton } from "./edit-employee-button";
 
-export const employeeColumns: ColumnDef<{
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  department: string;
-}>[] = [
+export const employeeColumns: ColumnDef<TEmployee>[] = [
   {
-    accessorKey: "firstName",
+    accessorKey: "full_name",
     header: "Nome",
   },
   {
@@ -25,14 +19,29 @@ export const employeeColumns: ColumnDef<{
   },
   {
     accessorKey: "department",
-    header: "Departamento",
+    cell: ({ row }) => (
+      <div className="line-clamp-1">{row.original.department.name}</div>
+    ),
   },
   {
     accessorKey: "actions",
     header: () => <div className="text-center">Ações</div>,
-    cell: () => (
+    cell: ({
+      row: {
+        original: { department, full_name, ...employee },
+      },
+    }) => (
       <div className="text-center">
-        <EditEmployeeButton />
+        <EditEmployeeButton
+          employee={{
+            ...employee,
+            department_name: department.name,
+            department_id: department.id.toString(),
+            confirm_password: "",
+            password: "",
+          }}
+          id={employee.id}
+        />
         <DeleteEmployeeButton />
       </div>
     ),
