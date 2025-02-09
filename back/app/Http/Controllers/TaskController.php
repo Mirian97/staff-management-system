ced828024dc0b7a7813837d98a49874e45060ff6
Mirian Quispe <mirianquispe1505@gmail.com>
2025-02-09 16:45:31 -0300

    feat: set index to receive a params search

--- back/app/Http/Controllers/TaskController.php
+++ back/app/Http/Controllers/TaskController.php
@@ -4,15 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TaskRequest;
 use App\Models\Task;
+use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $tasks = Task::with("employee")->paginate(10);
+        $search = "%{$request->query(key: 'search')}%";
+
+        $tasks = Task::where(function ($query) use ($search) {
+            $query->whereLike('id', $search)
+                ->orWhereLike('title', $search)
+                ->orWhereLike('description', $search)
+                ->orWhereRelation('employee', 'first_name', 'like', $search)
+                ->orWhereRelation('employee', 'last_name', 'like', $search);
+        })
+            ->with('employee')
+            ->paginate(10);
 
         return response()->json($tasks);
     }
