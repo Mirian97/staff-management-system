b5bf79ce419337997c27811d0d9b3457743ed4c1
Mirian Quispe <mirianquispe1505@gmail.com>
2025-02-15 22:25:00 -0300

    feat: include session provider for client components

--- front/app/_providers/all-providers.tsx
+++ front/app/_providers/all-providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 import { FC, PropsWithChildren } from "react";
 import { Toaster } from "sonner";
 import { queryClient } from "../_config/query-client";
@@ -7,8 +8,10 @@ import { queryClient } from "../_config/query-client";
 const AllProviders: FC<PropsWithChildren> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Toaster richColors position="top-center" />
-      {children}
+      <SessionProvider>
+        <Toaster richColors position="top-center" />
+        {children}
+      </SessionProvider>
     </QueryClientProvider>
   );
 };
