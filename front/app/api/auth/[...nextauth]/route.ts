7aecebe55093adbe4e171288e5a7bab897ab3752
Mirian Quispe <mirianquispe1505@gmail.com>
2025-02-15 22:25:45 -0300

    feat: define login with credentials

---
+++ front/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,43 @@
+import { api } from "@/app/_config/api";
+import { TToken } from "@/app/_types/auth-type";
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const { data } = await api.post<TToken>("/login", credentials);
+        if (!data) {
+          return null;
+        }
+        return data;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.accessToken = user.token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
